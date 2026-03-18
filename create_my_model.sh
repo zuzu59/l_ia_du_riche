2f1ec3b0309c16046dc01ef25759c3667bed67c2
Christian Zufferey <christian@zufferey.com>
2026-03-18 12:01:36 +0100

    new: pleins de nouveaux models files

--- create_my_model.sh
+++ create_my_model.sh
@@ -2,14 +2,19 @@
 
 # Petit script pour créer des nouveaux modèles avec une fenêtre de contexte propre à eux
 #
-#zf260207.2014
+#zf260207.2053, zf260306.1911
 
 read -p "Etes-vous certain de vouloir créer ces nouveaux modèles ?"
 
 cp *.modelfile ./ollama
 
-docker exec -it ollama ollama create gpt-oss-20b-128k -f /root/.ollama/gpt-oss-20b-128k.modelfile
-docker exec -it ollama ollama create qwen3-coder-30b-55k -f /root/.ollama/qwen3-coder-30b-55k.modelfile
-docker exec -it ollama ollama create devstral-small-2-24b-55k -f /root/.ollama/devstral-small-2-24b-55k.modelfile
+#docker exec -it ollama ollama create qwen3-coder-30b-55k -f /root/.ollama/qwen3-coder-30b-55k.modelfile
+#docker exec -it ollama ollama create devstral-small-2-24b-55k -f /root/.ollama/devstral-small-2-24b-55k.modelfile
+#docker exec -it ollama ollama create devstral-small-2-24b-128k -f /root/.ollama/devstral-small-2-24b-128k.modelfile
+docker exec -it ollama ollama create ministral-3:8b-wctx -f /root/.ollama/ministral-3:8b-wctx.modelfile
+docker exec -it ollama ollama create ministral-3:14b-wctx -f /root/.ollama/ministral-3:14b-wctx.modelfile
+docker exec -it ollama ollama create gpt-oss-20b-wctx -f /root/.ollama/gpt-oss-20b-wctx.modelfile
+docker exec -it ollama ollama create qwen3.5-9b-wctx -f /root/.ollama/qwen3.5-9b-wctx.modelfile
+docker exec -it ollama ollama create qwen3-vl:8bwctx -f /root/.ollama/qwen3-vl:8b-wctx.modelfile
 
 docker exec -it ollama ollama list
