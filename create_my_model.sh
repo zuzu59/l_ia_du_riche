799fa1ea836137d8cbf38050bb937104b4def839
Christian Zufferey <christian@zufferey.com>
2026-03-21 11:11:45 +0100

    refact: ajouté des autres modèles pour tests

--- create_my_model.sh
+++ create_my_model.sh
@@ -2,19 +2,18 @@
 
 # Petit script pour créer des nouveaux modèles avec une fenêtre de contexte propre à eux
 #
-#zf260207.2053, zf260306.1911
+#zf260207.2053, zf260321.0930
 
 read -p "Etes-vous certain de vouloir créer ces nouveaux modèles ?"
 
+rm ./ollama/*.modelfile
 cp *.modelfile ./ollama
 
-#docker exec -it ollama ollama create qwen3-coder-30b-55k -f /root/.ollama/qwen3-coder-30b-55k.modelfile
-#docker exec -it ollama ollama create devstral-small-2-24b-55k -f /root/.ollama/devstral-small-2-24b-55k.modelfile
-#docker exec -it ollama ollama create devstral-small-2-24b-128k -f /root/.ollama/devstral-small-2-24b-128k.modelfile
 docker exec -it ollama ollama create ministral-3:8b-wctx -f /root/.ollama/ministral-3:8b-wctx.modelfile
 docker exec -it ollama ollama create ministral-3:14b-wctx -f /root/.ollama/ministral-3:14b-wctx.modelfile
 docker exec -it ollama ollama create gpt-oss-20b-wctx -f /root/.ollama/gpt-oss-20b-wctx.modelfile
 docker exec -it ollama ollama create qwen3.5-9b-wctx -f /root/.ollama/qwen3.5-9b-wctx.modelfile
-docker exec -it ollama ollama create qwen3-vl:8bwctx -f /root/.ollama/qwen3-vl:8b-wctx.modelfile
+docker exec -it ollama ollama create nemotron-3-nano:4b-wctx -f /root/.ollama/nemotron-3-nano:4b-wctx.modelfile
+docker exec -it ollama ollama create toto-wctx -f /root/.ollama/glm-4.7-flash-q4_K_M-wctx.modelfile
 
 docker exec -it ollama ollama list
