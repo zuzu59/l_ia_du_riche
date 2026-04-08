2efff7fb6f195e26fcdf4299807ed8170c212772
root <root@ai-web.zuzu59.ch>
2026-04-08 09:42:59 +0200

    new: ajouté le modèle gemma4:e2b pour une utilisation sur CPU

--- create_my_model.sh
+++ create_my_model.sh
@@ -2,7 +2,7 @@
 
 # Petit script pour créer des nouveaux modèles avec une fenêtre de contexte propre à eux
 #
-#zf260207.2053, zf260321.0930
+#zf260207.2053, zf260408.0936
 
 read -p "Etes-vous certain de vouloir créer ces nouveaux modèles ?"
 
@@ -15,5 +15,6 @@ docker exec -it ollama ollama create gpt-oss-20b-wctx -f /root/.ollama/gpt-oss-2
 docker exec -it ollama ollama create qwen3.5-9b-wctx -f /root/.ollama/qwen3.5-9b-wctx.modelfile
 docker exec -it ollama ollama create nemotron-3-nano:4b-wctx -f /root/.ollama/nemotron-3-nano:4b-wctx.modelfile
 docker exec -it ollama ollama create toto-wctx -f /root/.ollama/glm-4.7-flash-q4_K_M-wctx.modelfile
+docker exec -it ollama ollama create gemma4-e2b-wctx -f /root/.ollama/gemma4-e2b-wctx.modelfile
 
 docker exec -it ollama ollama list
