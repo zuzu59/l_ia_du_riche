4a2d28c147ff9ca000174021910e7755f80f40a5
Christian Zufferey <christian@zufferey.com>
2026-04-21 13:51:41 +0200

    new: ajouté un modèle qwen3.5:2b-128k et un nouveau create_my_model pour la NVIDIA 10GB VRAM

---
+++ create_my_model-10GB.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Petit script pour créer des nouveaux modèles avec une fenêtre de contexte propre à eux
+#
+#zf260207.2053, zf260421.1345
+
+read -p "Etes-vous certain de vouloir créer ces nouveaux modèles ?"
+
+rm ./ollama/*.modelfile
+cp *.modelfile ./ollama
+
+#docker exec -it ollama ollama create ministral-3:8b-wctx -f /root/.ollama/ministral-3:8b-wctx.modelfile
+#docker exec -it ollama ollama create ministral-3:14b-wctx -f /root/.ollama/ministral-3:14b-wctx.modelfile
+#docker exec -it ollama ollama create nemotron-3-nano:4b-wctx -f /root/.ollama/nemotron-3-nano:4b-wctx.modelfile
+#docker exec -it ollama ollama create toto-wctx -f /root/.ollama/glm-4.7-flash-q4_K_M-wctx.modelfile
+#docker exec -it ollama ollama create z-gpt-oss-20b-128k -f /root/.ollama/gpt-oss-20b-wctx.modelfile
+#docker exec -it ollama ollama create z-qwen3-5-9b-256k -f /root/.ollama/qwen3.5-9b-wctx.modelfile
+docker exec -it ollama ollama create z-qwen3-5-2b-128k -f /root/.ollama/qwen3.5-2b-wctx.modelfile
+docker exec -it ollama ollama create z-gemma4-e2b-128k -f /root/.ollama/gemma4-e2b-wctx.modelfile
+#docker exec -it ollama ollama create z-gemma4-e4b-128k -f /root/.ollama/gemma4-e4b-wctx.modelfile
+
+docker exec -it ollama ollama list
