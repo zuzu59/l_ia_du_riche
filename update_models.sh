4e2a76d0b99ca119c1b3d8837be5d788cc8647fc
Christian Zufferey <christian@zufferey.com>
2026-05-15 16:29:43 +0200

    refact: amélioré les updates des llm sur Ollama

--- update_models.sh
+++ update_models.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 #Petit script pour passer les updates facilement de tous les modèles d'un coup de Ollama
-#zf251013.1731, zf251013.1735
+#zf251013.1731, zf260515.1528
 
 
 docker exec -it ollama sh -c 'ollama list | awk '\''NR>1 && !/reviewer/ {print $1}'\'' | xargs -n1 ollama pull'
 ./restart.sh
+
+docker exec -i ollama ollama list | sort
+
 
